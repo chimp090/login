13f849f8d340f997090085a2c37a6b2e09935c61
chimp090 <114457885+chimp090@users.noreply.github.com>
2022-09-26 21:56:50 -0500

    Create firstcode.py

---
+++ firstcode.py
@@ -0,0 +1,24 @@
+user = input('what is your name: ')
+user2 = input('what is your name: ')
+password = input('what is your password: ')
+password2 = input('what is your password: ')
+a = user
+b = user2
+c = password
+d = password2
+if a == 'matt':
+    print("Hello, matt")
+if a != 'matt':
+    print("error: name")
+if b == 'andrew':
+    print("Hello, andrew")
+if b != 'andrew':
+    print("error: name")
+if c == '12345':
+    print("Welcome, matt")
+if c != '12345':
+    print("error: password")
+if d == '67890':
+    print("Welcome, andrew")
+if d != '67890':
+    print("error: password")
